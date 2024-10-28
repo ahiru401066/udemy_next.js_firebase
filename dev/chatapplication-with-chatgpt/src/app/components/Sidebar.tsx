61bf9f62af7a5c2e0c2604c2d9cf2217d128fb44
Tomoya <ichikawatomoya0705@gmail.com>
2024-10-28 23:49:23 +0900

    クライアント側からデータの呼び出し可能

--- dev/chatapplication-with-chatgpt/src/app/components/Sidebar.tsx
+++ dev/chatapplication-with-chatgpt/src/app/components/Sidebar.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+"use client";
+
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect } from 'react'
 import { BiLogOut } from "react-icons/bi";
+import{ db } from "../../../firebase";
 
 const Sidebar = () => {
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const roomCollectionRef = collection(db,"rooms");
+      const q = query(roomCollectionRef, orderBy("createdAt"));
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const newRooms = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(newRooms);
+      });
+    };
+
+    fetchRooms();
+  },[]);
+
+
+
   return (
     <div className="bg-cyan-950 h-full overflow-y-auto px-5 flex flex-col">
       <div className="flex-grow">
